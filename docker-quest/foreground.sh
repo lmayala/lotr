df654a5dea58b124a414e5e706335997ccdfd16c
Leo Mendez <lmaya03@gmail.com>
2026-05-16 18:32:38 -0300

    Fix killercoda foreground for building locally

--- docker-quest/foreground.sh
+++ docker-quest/foreground.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-docker run -d \
-  --privileged \
-  --name lotr \
-  lmayala/lotr
+git clone git@github.com:lmayala/ring-quest.git
+docker build -t lotr .
+docker run -d --privileged --name lotr lotr
 
 until docker exec lotr true >/dev/null 2>&1
 do
